f5951cdb4c684936d955c4d326e8ae3289c76e8d
Karla <karla.perezn96@alumnostrazos.net>
2026-04-24 18:35:02 +0200

    24Abr_5

--- index.js
+++ index.js
@@ -1,24 +1,39 @@
-function date() {
+function inputDate() {
     let year = getId("year").value;
     let month = getId("month").value
     let day = getId("day").value
 
     let date = `${year}-${month}-${day}`;
 
-    astronomyImg(date)
+    astronomyImg(date, "imgAstronomySearch")
 }
 
+function todayDate() {
+    const hoy = new Date();
 
-function astronomyImg(date) {
-    
+    let year = hoy.getFullYear();
+    let month = String(hoy.getMonth() + 1).padStart(2, '0');
+    let day = String(hoy.getDate()).padStart(2, '0');
+
+
+    let date = `${year}-${month}-${day}`;
+
+    astronomyImg(date, "hero")
+}
+
+function astronomyImg(date, id) {
 
     const nasaKey = "IIYyn4oPoU1LOKO0qz0JDdgTaKSI0m1GfNeUysTk"
     fetch(`https://api.nasa.gov/planetary/apod?api_key=${nasaKey}&date=${date}`).then(res => res.json()).then(data => {
-    console.log(data)
+        getId(id).style.backgroundImage = `url(${data.url})`;
+        getId("titleImg").innerText = data.title;
+        getId(id).innerHTML = `<img src=${data.url}>`
+
+
 })
 }
 
-astronomyImg("2015-10-08")
+todayDate ()
 
 function getId(id) {
             return document.getElementById(id)
